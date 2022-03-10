d0b2517593cf07c7847811f1e8524fdcc16a3522
visotoniki9 <d.simon.ssx0@gmail.com>
2022-03-10 22:39:49 +0100

    Added Banner

---
+++ components/Banner.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Banner() {
+  return (
+    <div className="bg-[#FFC017] border-t border-b border-black h-[455px]">
+      <div className="flex flex-col justify-center max-w-7xl mx-auto px-12 h-[455px]">
+        <h1 className="text-8xl font-serif">Stay curious.</h1>
+        <h3 className="text-3xl">
+          Discover stories, thinking, and expertise
+          <h3>from writers on any topic.</h3>
+        </h3>
+      </div>
+    </div>
+  )
+}
+
+export default Banner
