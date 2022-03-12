ea62ecaf17e038d80c5f317bc89be0fba3a3f87f
visotoniki9 <d.simon.ssx0@gmail.com>
2022-03-12 16:16:34 +0100

    Posts component implemented

---
+++ components/Posts.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import { sanityClient, urlFor } from '../sanity'
+
+function Posts({ posts }) {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-5 cursor-pointer">
+      {posts.map((post) => (
+        <Link key={post._id} href={`/post/${post.slug.current}`} passHref>
+          {/* Post  */}
+          <div className="shadow-md overflow-hidden border rounded-lg group">
+            <img
+              className="group-hover:scale-105 transition ease-in-out"
+              src={urlFor(post.mainImage).url()!}
+            />
+            <div className="flex space-y-2 bg-white justify-between p-2">
+              <p className="font-semibold">{post.title}</p>
+              <img
+                className="w-12 h-12 rounded-full"
+                src={urlFor(post?.author?.image).url()!}
+              />
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+  )
+}
+
+export default Posts
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,39 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
+import { sanityClient } from '../sanity'
 import Banner from '../components/Banner'
 import Header from '../components/Header'
+import Link from 'next/link'
+import Posts from '../components/Posts'
 
-const Home: NextPage = () => {
-  return (
-    <div>
-      <Head>
-        <title>Medium 2.0</title>
-        <link rel="icon" href="/Medium.svg" />
-      </Head>
-      <Header />
-      <Banner />
-    </div>
-  )
-}
+const Home: NextPage = ({ posts }) => (
+  <div>
+    <Head>
+      <title>Medium 2.0</title>
+      <link rel="icon" href="/Medium.svg" />
+    </Head>
+    <Header />
+    <Banner />
+    <Posts posts={posts} />
+  </div>
+)
 
 export default Home
+
+export const getServerSideProps = async () => {
+  const query = /* groq */ `*[_type=="post"]{
+    _id,
+    title,
+    author->{
+      name,
+      image
+    },
+    description,
+    mainImage,
+    slug
+  }`
+
+  const posts = await sanityClient.fetch(query)
+
+  return { props: { posts } }
+}
